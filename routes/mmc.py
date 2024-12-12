6e38c38411c68f0ff2f6a3a2d8e58962d7ed5352
YAMADA-57 <k23057kk@aitech.ac.jp>
2024-12-12 15:53:57 +0900

    メイン画面接続時のテスト表示

--- routes/mmc.py
+++ routes/mmc.py
@@ -3,5 +3,41 @@ from models import Order, User, Product
 
 
 # Blueprintの作成
-order_bp = Blueprint('order', __name__, url_prefix='/orders')
+mmcgraph_bp = Blueprint('mmc_graph', __name__, url_prefix='http://127.0.0.1:8080/')
 
+print("test")
+
+# productID と userID のデータが必要
+
+@mmcgraph_bp.route(methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        user_id = request.form['name']
+        
+        # modelsから価格を変数に代入
+        money = request.form['price']
+        
+        print("--=====",money,"--=====")
+        
+        Order.create(user=user_id, money = money)
+        
+        return redirect(url_for('order.list'))
+    users = User.select()
+    moneys = Product.select()
+    return render_template('order_add.html', users=users, products=moneys)
+
+# @order_bp.route('/edit/<int:order_id>', methods=['GET', 'POST'])
+# def edit(order_id):
+#     order = Order.get_or_none(Order.id == order_id)
+#     if not order:
+#         return redirect(url_for('order.list'))
+
+#     if request.method == 'POST':
+#         order.user = request.form['user_id']
+#         order.product = request.form['product_id']
+#         order.save()
+#         return redirect(url_for('order.list'))
+
+#     users = User.select()
+#     products = Product.select()
+#     return render_template('order_edit.html', order=order, users=users, products=products)
