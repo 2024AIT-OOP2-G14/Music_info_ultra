45f252856fe192ff9b0cc004715346702dac4fe4
hiy-hiy <k23106kk@aitech.ac.jp>
2024-12-12 15:56:07 +0900

    「会員登録の年間推移」についての変更

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,9 +1,11 @@
+from .register_chart import register_bp
 from .user import user_bp
 from .product import product_bp
 from .order import order_bp
 
 # Blueprintをリストとしてまとめる
 blueprints = [
+  register_bp,
   user_bp,
   product_bp,
   order_bp
