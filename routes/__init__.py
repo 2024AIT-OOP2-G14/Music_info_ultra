58bfe8ed653c06d51476d7ddd9c9d9a9d9880598
k23061kk <k23061kk@aitech.ac.jp>
2024-12-17 13:39:07 +0900

    Add:get_ranking_data
    アルバムの貸し出し枚数ランキングのデータを取得し、貸し出し枚数の降順にして返すget_ranking_data()の中身を記述しました。

--- routes/__init__.py
+++ routes/__init__.py
@@ -2,7 +2,9 @@ from .register_chart import register_bp
 from .user import user_bp
 from .product import product_bp
 from .order import order_bp
-#from .album_units_sold import album_units_sold_bp
+
+from .album_units_sold import ranking_bp  # すでに import 済み
+
 
 # Blueprintをリストとしてまとめる
 blueprints = [
@@ -10,5 +12,5 @@ blueprints = [
   user_bp,
   product_bp,
   order_bp,
-  #album_units_sold_bp
+  ranking_bp  # 人気アルバムランキングの Blueprint を追加
 ]
